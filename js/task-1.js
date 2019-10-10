1575e75097fcf8c2881f9533d4291f07134d8825
MykhailoMelnichuk <mmdcomp@ukr.net>
2019-10-10 18:09:25 +0300

    HW-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 const name = 'Генератор защитного поля';
 let prise = 1000;
 
-console.log('Выбран «' + name + '», цена за штуку ' + prise + ' кредитов')
+console.log(`Выбран «${name}», цена за штуку ${prise} кредитов.`)
 
 prise = 2000;
 
-console.log('Выбран «' + name + '», цена за штуку ' + prise + ' кредитов')
+console.log(`Выбран «${name}», цена за штуку ${prise} кредитов.`)
--- js/task-6.js
+++ js/task-6.js
@@ -1,17 +1,12 @@
-let input =0;
+let input = 0;
 let total = 0;
-let proverka;
-
 while (input !== null) {
-    console.log(input);
     input = Number(input);
-    console.log(input);
     if (Number.isNaN(input)) {
         alert('Было введено не число, попробуйте еще раз.')
         input = 0;
     };
     total = total + input;
-    console.log(total);
     input = prompt('Введите число:');
 }
 alert(`Общая сумма чисел равна ${total}.`)
